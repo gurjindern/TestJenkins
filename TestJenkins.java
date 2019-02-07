abe0e0ff9a16b8a864a6f51e352fbe3e60a9c277
gurjinder <gurjinder@cannforecast.com>
2019-02-07 13:00:19 -0500

    3rd commmit

---
+++ TestJenkins.java
@@ -0,0 +1,20 @@
+
+package guri;
+
+
+public class TestJenkins{
+
+	public static void main(String[] args){
+		
+		for(String arg: args){
+		
+			System.out.println("value for arg is....:"+arg+"  hahahahha");
+		}
+
+
+	}
+
+
+
+
+}
